7576ee628376e660499899269173b2938bb5a52c
AndyOmondi17 <andyomondi17@gmail.com>
2021-07-10 09:54:03 +0300

    make the layout

--- app/src/main/java/com/andy/androidcat/customFragment.java
+++ app/src/main/java/com/andy/androidcat/customFragment.java
@@ -1,5 +1,23 @@
 package com.andy.androidcat;
 
 
-public class customFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class customFragment extends ArFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+//       will hide the animation
+        FrameLayout frameLayout = (FrameLayout) super.onCreateView(inflater, container, savedInstanceState);
+        getPlaneDiscoveryController().hide();
+        getPlaneDiscoveryController().setInstructionView();
+
+        return frameLayout;
+    }
 }
